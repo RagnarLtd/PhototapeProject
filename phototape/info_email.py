f4dd2fb1f6b4f9e69cbd2972140f53ba8dd3f968
RagnarLtd <79021347+RagnarLtd@users.noreply.github.com>
2022-08-12 00:28:05 +0300

    Update info_email.py

--- phototape/info_email.py
+++ phototape/info_email.py
@@ -1,6 +1,6 @@
 EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_HOST_USER = 'greenmentRagnar@yandex.ru'
-EMAIL_HOST_PASSWORD = 'iqhekidhmvqgqxgi'
+EMAIL_HOST_USER = 'test@test.ru'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 465
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
